b66d00b18d70779d47201e5359102dd426169e2b
steven <gitmail@m20.nl>
2022-02-20 14:37:21 +0100

    fix 13.3 spm issue

--- Package.swift
+++ Package.swift
@@ -15,11 +15,11 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "ZendeskMessagingAPISDK",
-                 url: "https://github.com/zendesk/messagingapi_sdk_ios",
-                 .exact("3.8.3")),
+                 url: "https://github.com/stevenkramer/messagingapi_sdk_ios",
+                 .exact("3.8.4")),
         .package(name: "ZendeskCommonUISDK",
-                 url: "https://github.com/zendesk/commonui_sdk_ios",
-                 .exact("6.1.2"))
+                 url: "https://github.com/stevenkramer/commonui_sdk_ios",
+                 .exact("6.1.3"))
     ],
     targets: [
         .binaryTarget(
